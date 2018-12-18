5bc515f5a380b694990288fe2a110b7e1f5f2070
Meenal <meenalalagappan89@gmail.com>
2018-12-17 20:53:51 -0800

    changes

--- js/app.js
+++ js/app.js
@@ -7,24 +7,12 @@ $(function () {
             var url = "mail.php";
 
             $.ajax({
-                type: "POST",
-                url: url,
-                data: $(this).serialize(),
-                success: function (data)
-                {
-                    var messageAlert = 'alert-' + data.type;
-                    var messageText = data.message;
-
-                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                    if (messageAlert && messageText) {
-                        $('#contact-form').find('.messages').html(alertBox);
-                        $('#contact-form')[0].reset();
-                    }
-                }
+                "email": "mytestemail-pm45467t76",
+                "phone": "575768757",
+                "message": "huguhufj ugyugug thyjfhtdrfh",
+                "name": "bad boy"
             });
-            return false;
         }
     })
 });
-
 
