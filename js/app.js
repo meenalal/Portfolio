5793c93295729b03b9db712fd373de55f3cb31a0
alag9571 <a.alagappan@esri.com>
2018-12-18 10:45:26 -0800

    call to send contact

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
     $("#contactform").submit(function (event) {
         if ($("input:first").val() === "correct") {
-            $.post("https://2jadto2g2i.execute-api.us-east-1.amazonaws.com/default/sendcontactemail", {
+            $.post("https://2jadto2g2i.execute-api.us-east-1.amazonaws.com/default/sendcontactemail", `{
                 "email": "mytestemail-454545",
                 "phone": "575768757",
                 "message": "huguhufj ugyugug thyjfhtdrfh",
                 "name": "bad boy"
-            })
+            }`)
             $("#contactformres").text("Message sent...").show().fadeOut(2000);
             event.preventDefault();
             return false;
