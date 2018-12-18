70abee7fb587ddbc7f373708ffc0b8e71e029961
Meenal <meenalalagappan89@gmail.com>
2018-12-17 20:46:15 -0800

    changes

--- js/app.js
+++ js/app.js
@@ -1,57 +1,30 @@
-$(function() {
-    // Get the form.
-    var form = $('#ajax-contact');
+$(function () {
 
-    // Get the messages div.
-    var formMessages = $('#form-messages');
+    $('#contact-form').validator();
 
-    // TODO: The rest of the code will go here...
-});
+    $('#contact-form').on('submit', function (e) {
+        if (!e.isDefaultPrevented()) {
+            var url = "mail.php";
 
-// Set up an event listener for the contact form.
-$(form).submit(function(event) {
-    // Stop the browser from submitting the form.
-    event.preventDefault();
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $(this).serialize(),
+                success: function (data)
+                {
+                    var messageAlert = 'alert-' + data.type;
+                    var messageText = data.message;
 
-    // TODO
-});
-
-// Serialize the form data.
-var formData = $(form).serialize();
-
-// Submit the form using AJAX.
-$.ajax({
-    type: 'POST',
-    url: $(form).attr('action'),
-    data: formData
-})
-
-.done(function(response) {
-    // Make sure that the formMessages div has the 'success' class.
-    $(formMessages).removeClass('error');
-    $(formMessages).addClass('success');
-
-    // Set the message text.
-    $(formMessages).text(response);
-
-    // Clear the form.
-    $('#name').val('');
-    $('#email').val('');
-    $('#phone').val('');
-    $('#message').val('');
-})
-
-.fail(function(data) {
-    // Make sure that the formMessages div has the 'error' class.
-    $(formMessages).removeClass('success');
-    $(formMessages).addClass('error');
-
-    // Set the message text.
-    if (data.responseText !== '') {
-        $(formMessages).text(data.responseText);
-    } else {
-        $(formMessages).text('Oops! An error occured and your message could not be sent.');
-    }
+                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    if (messageAlert && messageText) {
+                        $('#contact-form').find('.messages').html(alertBox);
+                        $('#contact-form')[0].reset();
+                    }
+                }
+            });
+            return false;
+        }
+    })
 });
 
 
