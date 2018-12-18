e10a7c1edc6df9ee7921ad2f0182ffc11d54caf0
alag9571 <a.alagappan@esri.com>
2018-12-18 11:50:53 -0800

    call to send contact

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
 $(document).ready(function () {
     $("#contactform").submit(function (event) {
-        if ($("input:first").val() === "correct") {
+        event.preventDefault();
+        var name = $("input[name='Name']", this).val();
+        var email = $("input[name='Email']", this).val();
+        var phone = $("input[name='Phone']", this).val();
+        var message = $("input[name='Message']", this).val();
+
+        if (email !== "") {
             $.post("https://2jadto2g2i.execute-api.us-east-1.amazonaws.com/default/sendcontactemail", `{
-                "email": "mytestemail-454545",
-                "phone": "575768757",
-                "message": "huguhufj ugyugug thyjfhtdrfh",
-                "name": "bad boy"
+                "email": "${email}",
+                "phone": "${phone}",
+                "message": "${message}",
+                "name": "${name}"
             }`)
             $("#contactformres").text("Message sent...").show().fadeOut(2000);
-            event.preventDefault();
+
             return false;
         }
 
