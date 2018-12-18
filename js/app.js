b79a8854ea0a1cf9b49374e7aedf7144aecf3663
alag9571 <a.alagappan@esri.com>
2018-12-18 12:51:10 -0800

    call to send contact

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
         var email = $("input[name='Email']", this).val();
         var phone = $("input[name='Phone']", this).val();
         var message = $("textarea[name='Message...']", this).val();
+        message = message.replace(/["']/g, "")
 
         if (email !== "") {
             $.post("https://2jadto2g2i.execute-api.us-east-1.amazonaws.com/default/sendcontactemail", `{
